86a04d1e7c8729cf1300c4d6d8515d8c2111041a
frknlke <furkan.ulke@boun.edu.tr>
2024-03-11 11:19:00 +0300

    Added functions to extract info from description

---
+++ product.py
@@ -0,0 +1,9 @@
+# product.py
+import product_details
+
+class Product:
+    def __init__(self, product_name, price, discounted_price, product_type, quantity, color, series, season):
+        self.product_details = product_details.ProductDetails(price, discounted_price, product_type, quantity, color, series, season)
+        # self.stock_code = None handle it according to the answer
+        # self.name = product_name handle it. Must be uppercase
+
